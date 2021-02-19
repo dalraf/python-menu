f4e4ba5b1ac1978b987e749ae199189a7ff85101
Daniel Fonseca Alves <dalraf@gmail.com>
2021-02-19 17:29:45 -0300

    Instalar chocolatey

--- python-menu.py
+++ python-menu.py
@@ -1,33 +1,24 @@
-# Import the necessary packages
 from consolemenu import *
 from consolemenu.items import *
+import subprocess
 
-# Create the menu
-menu = ConsoleMenu("Title", "Subtitle")
 
-# Create some items
+def run(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
 
-# MenuItem is the base class for all items, it doesn't do anything when selected
-menu_item = MenuItem("Menu Item")
+def installchocolatey():
+    run("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))')")
+    return completed
 
-# A FunctionItem runs a Python function when selected
-function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
+menu = ConsoleMenu("Configurador de Destops", "Script para configuração padrao de desktops")
+menu_item = MenuItem("Menu")
 
-# A CommandItem runs a console command
-command_item = CommandItem("Run a console command",  "touch hello.txt")
+InstalarChocolatey= FunctionItem("Instalar Chocolatey", installchocolatey)
 
-# A SelectionMenu constructs a menu from a list of strings
-selection_menu = SelectionMenu(["item1", "item2", "item3"])
 
-# A SubmenuItem lets you add a menu (the selection_menu above, for example)
-# as a submenu of another menu
-submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
-
-# Once we're done creating them, we just add the items to the menu
 menu.append_item(menu_item)
-menu.append_item(function_item)
-menu.append_item(command_item)
-menu.append_item(submenu_item)
+menu.append_item(InstalarChocolatey)
 
 # Finally, we call show to show the menu and allow the user to interact
 menu.show()
